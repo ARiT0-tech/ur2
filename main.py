1573f1f5271efe40d0d8dea522a87fd6f8b4cb39
Artem Ananev <ananev.artem272005@gmail.com>
2021-03-04 16:28:13 +0300

    com2

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from flask import Flask
 from data import db_session
 from data.users import User
 from data.info import info
+from data.jobs import Jobs
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
 
 
 def main():
@@ -24,43 +24,12 @@ def main():
         db_sess.add(user)
         db_sess.commit()
 
-#
-    # user1 = User()
-    # user1.surname = "Ridley"
-    # user1.name = "Max"
-    # user1.age = 21
-    # user1.position = "colonist"
-    # user1.speciality = "research engineer"
-    # user1.address = "module_2"
-    # user1.email = "MaxMars@mars.org"
-    # user1.hashed_password = "max"
-    # db_sess.add(user1)
-    # db_sess.commit()
-#
-    # user2 = User()
-    # user2.surname = "Cook"
-    # user2.name = "Bob"
-    # user2.age = 21
-    # user2.position = "cook"
-    # user2.speciality = "cook"
-    # user2.address = "module_2"
-    # user2.email = "book.cook@mars.org"
-    # user2.hashed_password = "bobcok"
-    # db_sess.add(user2)
-    # db_sess.commit()
-#
-    # user3 = User()
-    # user3.surname = "Scott"
-    # user3.name = "Adam"
-    # user3.age = 21
-    # user3.position = "doctor"
-    # user3.speciality = "doctor"
-    # user3.address = "module_2"
-    # user3.email = "scott_doc@mars.org"
-    # user3.hashed_password = "doc"
-    # db_sess.add(user3)
-    # db_sess.commit()
-
+    jobs = Jobs()
+    jobs.team_leader = 1
+    jobs.job = 'deployment of residential modules 1 and 2'
+    jobs.work_size = 15
+    jobs.collaborators = '2, 3'
+    jobs.is_finished = False
     app.run()
 
 
